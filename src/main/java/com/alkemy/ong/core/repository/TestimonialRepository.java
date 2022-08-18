82f15ab6e11bed544d5ce00db6497932fac3f651
Juacun <juanpablo.cundere@gmail.com>
2022-08-18 20:10:40 -0300

    Creation of TestimonialRepository in repository package

---
+++ src/main/java/com/alkemy/ong/core/repository/TestimonialRepository.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.core.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.alkemy.ong.core.model.Testimonial;
+
+public interface TestimonialRepository extends CrudRepository<Testimonial, Long> {
+
+}
