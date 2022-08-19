7c8dce70076b8d5716860b8376ae694d6c33ac6b
FedeOA <fedeacosta07@hotmail.com>
2022-08-19 10:22:02 -0300

    Contact class creation in model AND add contact table creation script

---
+++ src/main/java/com/alkemy/ong/core/model/Contact.java
@@ -0,0 +1,65 @@
+package com.alkemy.ong.core.model;
+
+import com.alkemy.ong.core.model.audit.Audit;
+import com.alkemy.ong.core.model.audit.AuditListener;
+import com.alkemy.ong.core.model.audit.Auditable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "contact")
+@Where(clause = "is_active=true")
+@SQLDelete(sql = "UPDATE contacts SET is_active=false, deleted_at=current_timestamp() where contact_id=?")
+@EntityListeners(AuditListener.class)
+public class Contact implements Auditable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contact_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int phone;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "message", nullable = false, columnDefinition = "TEXT")
+    private String message;
+
+    @Column
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime deletedAt;
+
+    @Embedded
+    private Audit audit;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(id, contact.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
