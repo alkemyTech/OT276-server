79677fe1bba84af176ef694222e044436545d58a
JuanPablo <52846984+Juacun@users.noreply.github.com>
2022-08-20 11:27:58 -0300

    [OT276-21] Crear modelo y repositorio base de Testimonials (#6)
    * Creation of Testimonial entity in model package
    
    * Creation of TestimonialRepository in repository package
    
    * Creation of SQL Script to create Testimonial table in resources/db/migration
    
    * Fix of some errors
    
    * Set of attribute fields in Testimonial to private

---
+++ src/main/java/com/alkemy/ong/core/repository/TestimonialRepository.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.core.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alkemy.ong.core.model.Testimonial;
+
+public interface TestimonialRepository extends JpaRepository<Testimonial, Long> {
+
+}
