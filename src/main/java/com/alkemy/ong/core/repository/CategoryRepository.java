802944280dd5248eab0cf192327d7b5f7e2c6c56
Juanchini23 <91612292+Juanchini23@users.noreply.github.com>
2022-08-20 19:19:49 -0300

    [OT276-19] Crear modelo y repositorio base de Categories (#2)
    * category modelo y repositorio
    
    * error solucionado
    
    * cambio de nombre requerido

---
+++ src/main/java/com/alkemy/ong/core/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.alkemy.ong.core.repository;
+
+import com.alkemy.ong.core.model.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+}
