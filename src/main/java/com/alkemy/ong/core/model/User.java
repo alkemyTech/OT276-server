3df268fe8dd281540c61feafb7dddd09457f133a
Nestor Flores <nestorchoele@gmail.com>
2022-08-22 19:03:28 -0300

    create user model

---
+++ src/main/java/com/alkemy/ong/core/model/User.java
@@ -0,0 +1,110 @@
+package com.alkemy.ong.core.model;
+
+import com.alkemy.ong.core.model.audit.Audit;
+import com.alkemy.ong.core.model.audit.AuditListener;
+import com.alkemy.ong.core.model.audit.Auditable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+@Where(clause = "is_active=true")
+@SQLDelete(sql = "UPDATE user SET is_active=false WHERE user_id=?")
+@EntityListeners(AuditListener.class)
+public class User implements Auditable, UserDetails {
+    @Id
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique=true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String photo;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "role_id", referencedColumnName = "role_id")
+    @ToString.Exclude
+    private Role role;
+
+    @Embedded
+    private Audit audit;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role != null) {
+            List<GrantedAuthority> roles = new ArrayList<>();
+            roles.add(new SimpleGrantedAuthority(role.toString()));
+        }
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return audit.getIsActive();
+    }
+}
