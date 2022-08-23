339ef64a33aaf6870d215e9e8f99aa4f1ddab440
Jonathan Manera <manerajona@gmail.com>
2022-08-23 10:47:28 -0300

    [OT276-39] Setup Amazon S3 SDK (#15)
    * [OT276-75] Setup Amazon S3 SDK
    
    COMO usuario administrador
    QUIERO subir imágenes en el sistema
    PARA mejorar la comunicación
    
    Criterios de aceptación:
    Se deberá vincular el servicio para manejo de archivos Amazon S3, a través de su SDK para Java. El mismo se utilizará posteriormente para campos de diferentes entidades, como la foto de perfil del usuario, logo de ONG, imágenes del blog y demás, por lo que debe crearse un servicio reutilizable para el manejo de archivos.
    Se deberá incluir la dependencia para el manejo de conexión con S3.

---
+++ src/main/java/com/alkemy/ong/ports/output/s3/S3ServiceImpl.java
@@ -0,0 +1,78 @@
+package com.alkemy.ong.ports.output.s3;
+
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class S3ServiceImpl implements S3Service{
+
+    private final AmazonS3 s3client;
+
+    @Value("${amazonProperties.bucketName:cohorte-agosto-38d749a7}")
+    private String bucketName;
+
+
+    @Override
+    public String uploadFile(MultipartFile multipartFile) {
+
+        String fileUrl;
+        try {
+
+            File file = convertMultiPartToFile(multipartFile);
+            String fileName = generateFileName(multipartFile);
+            fileUrl = s3client.getUrl(bucketName, fileName).toString();
+            uploadFileTos3bucket(fileName, file);
+            file.delete();
+
+        } catch (Exception e) {
+
+            log.error("error uploading file", e);
+            throw new RuntimeException(e);
+        }
+        return fileUrl;
+    }
+
+    @Override
+    public String uploadFile(String fileBase64, String fileName) {
+        //todo
+        return null;
+    }
+
+    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
+    }
+
+    private String generateFileName(MultipartFile multiPart) {
+        return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
+    }
+
+    private void uploadFileTos3bucket(String fileName, File file) {
+        s3client.putObject(new PutObjectRequest(bucketName, fileName, file)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+
+    public String deleteFileFromS3Bucket(String fileUrl) {
+        String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+        s3client.deleteObject(new DeleteObjectRequest(bucketName + "/", fileName));
+        return "Successfully deleted";
+    }
+}
+
