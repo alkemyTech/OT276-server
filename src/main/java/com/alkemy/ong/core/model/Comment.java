4ea4959160fa8aae6cc068d807245829dd506fef
Jonathan Manera <manerajona@gmail.com>
2022-08-23 15:50:09 -0300

    optimize imports

--- src/main/java/com/alkemy/ong/core/model/Comment.java
+++ src/main/java/com/alkemy/ong/core/model/Comment.java
@@ -1,21 +1,27 @@
 package com.alkemy.ong.core.model;
 
-import java.util.Objects;
-
-import javax.persistence.*;
-
-import org.apache.commons.lang3.builder.ToStringExclude;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
-
 import com.alkemy.ong.core.model.audit.Audit;
 import com.alkemy.ong.core.model.audit.AuditListener;
 import com.alkemy.ong.core.model.audit.Auditable;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -28,42 +34,42 @@ import lombok.ToString;
 @EntityListeners(AuditListener.class)
 public class Comment implements Auditable {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "comment_id")
-	private Long id;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id")
-	@ToString.Exclude
-	private User user;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "new_id")
-	@ToString.Exclude
-	private New _new;
-	
-	@Column(nullable = false, columnDefinition = "text")
-	private String body;
-	
-	@Embedded
-	private Audit audit;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Comment other = (Comment) obj;
-		return Objects.equals(id, other.id);
-	}
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-	
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @ToString.Exclude
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "new_id")
+    @ToString.Exclude
+    private New _new;
+
+    @Column(nullable = false, columnDefinition = "text")
+    private String body;
+
+    @Embedded
+    private Audit audit;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Comment other = (Comment) obj;
+        return Objects.equals(id, other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
