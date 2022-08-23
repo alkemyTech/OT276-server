ca41cf40106887298d6b5735a269e7c8b609d6dd
Nestor Flores <nestorchoele@gmail.com>
2022-08-23 09:57:00 -0300

    [OT276-18] Crear modelo y repositorio base de Users (#14)
    * create user table script
    
    * create user repository
    
    * create user model
    
    * athorities and fetch

---
+++ src/main/java/com/alkemy/ong/core/model/User.java
@@ -0,0 +1,116 @@
+package com.alkemy.ong.core.model;
+
+import com.alkemy.ong.core.model.audit.Audit;
+import com.alkemy.ong.core.model.audit.AuditListener;
+import com.alkemy.ong.core.model.audit.Auditable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+@Where(clause = "is_active=true")
+@SQLDelete(sql = "UPDATE user SET is_active=false WHERE user_id=?")
+@EntityListeners(AuditListener.class)
+public class User implements Auditable, UserDetails {
+
+    @Id
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String photo;
+
+    @OneToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "role_id", referencedColumnName = "role_id")
+    @ToString.Exclude
+    private Role role;
+
+    @Embedded
+    private Audit audit;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role != null) {
+            return Collections.singleton(this.role);
+        }
+        return Collections.emptySet();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return audit.getIsActive();
+    }
+}
