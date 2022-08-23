bf5bc056f1b9cbc06265dc9d0aae48ea7c3f6412
Nestor Flores <nestorchoele@gmail.com>
2022-08-23 20:22:32 -0300

    create jwt utils

--- src/main/java/com/alkemy/ong/config/security/JwtUtils.java
+++ src/main/java/com/alkemy/ong/config/security/JwtUtils.java
@@ -3,10 +3,13 @@ package com.alkemy.ong.config.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class JwtUtils {
@@ -15,14 +18,16 @@ public class JwtUtils {
     static final long EXPIRE_DURATION = 24 * 60 * 60 * 1000; // 24 hour
     protected final String HEADER = "Authorization";
     protected final String PREFIX = "Bearer ";
-
+//    private final String SECRET = "mySecretKey";
 
     protected Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public String getJWTToken(String username) {
+        List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                .commaSeparatedStringToAuthorityList("ROLE_USER");
 
         String token = Jwts.builder()
-                .setSubject(username)
+                .setSubject("Alkemy")
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRE_DURATION))
                 .signWith(SECRET_KEY)
