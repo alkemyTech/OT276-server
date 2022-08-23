6e5f7a8a9f44acc6245bc2a55617fc871a365973
Nestor Flores <nestorchoele@gmail.com>
2022-08-23 20:37:16 -0300

    add UserRepository

--- src/main/java/com/alkemy/ong/core/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/core/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.alkemy.ong.core.repository;
 import com.alkemy.ong.core.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    public Optional<User> findByEmail(String email);
 }
