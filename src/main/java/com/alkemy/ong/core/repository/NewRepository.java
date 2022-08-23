52b7db9ef6d9edef51c1ead9b7e50139d80ebf7d
Juanchini23 <91612292+Juanchini23@users.noreply.github.com>
2022-08-23 09:49:35 -0300

    [OT276-20] Crear modelo y repositorio base de News (#11)
    * New model, repository y table
    
    * importacion de clase requerida
    
    * exclude tostring

---
+++ src/main/java/com/alkemy/ong/core/repository/NewRepository.java
@@ -0,0 +1,7 @@
+package com.alkemy.ong.core.repository;
+
+import com.alkemy.ong.core.model.New;
+import org.springframework.data.repository.CrudRepository;
+
+public interface NewRepository extends CrudRepository<New, Long> {
+}
