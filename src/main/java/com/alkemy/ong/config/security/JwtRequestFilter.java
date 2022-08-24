a1f10ab00b563c76176a5874c8fe62fc5fef92d2
Nestor Flores <nestorchoele@gmail.com>
2022-08-23 22:14:11 -0300

    implement JwtRequestFilter

--- src/main/java/com/alkemy/ong/config/security/JwtRequestFilter.java
+++ src/main/java/com/alkemy/ong/config/security/JwtRequestFilter.java
@@ -1,4 +1,74 @@
 package com.alkemy.ong.config.security;
 
-public class JwtRequestFilter {
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Encoders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        try {
+            if (existeJWTToken(request)) {
+                Claims claims = validateToken(request);
+                if (claims.get("authorities") != null) {
+                    setUpSpringAuthentication(claims);
+                } else {
+                    SecurityContextHolder.clearContext();
+                }
+            } else {
+                SecurityContextHolder.clearContext();
+            }
+            chain.doFilter(request, response);
+        } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException e) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
+        }
+    }
+
+    private Claims validateToken(HttpServletRequest request) {
+
+        String secretString = Encoders.BASE64.encode(jwtUtils.SECRET_KEY.getEncoded());
+
+        request.getHeader(jwtUtils.HEADER).replace(jwtUtils.PREFIX, "");
+
+        return Jwts.parserBuilder()
+                .setSigningKey(jwtUtils.SECRET_KEY)
+                .build()
+                .parseClaimsJws(secretString).getBody();
+    }
+
+
+    private void setUpSpringAuthentication(Claims claims) {
+
+        List<String> authorities = (List) claims.get("authorities");
+
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(claims.getSubject(), null,
+                authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+    }
+
+    private boolean existeJWTToken(HttpServletRequest request) {
+        String authenticationHeader = request.getHeader(jwtUtils.HEADER);
+        return authenticationHeader != null && authenticationHeader.startsWith(jwtUtils.PREFIX);
+    }
+
 }
