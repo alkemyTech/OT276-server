4fc65ea171d2eb12d8a9b59beac20ccfbdd5bae0
Juanchini23 <jmanchini@alumno.unlam.edu.ar>
2022-08-25 11:46:30 -0300

    add organization script

---
+++ src/main/resources/db/migration/V1.65__add_organization.sql
@@ -0,0 +1,24 @@
+insert into organization(organization_id, name, image, address, phone, email, welcome_text, about_us_text, facebook, linkedin, instagram, created_at, is_active)
+        values (1,
+                "Somos Mas",
+                "LOGO-SOMOS MAS",
+                "Barrio La Cava",
+                1160112988,
+                "somosfundacionmas@gmail.com",
+                "¡Bienvenido a ONG Somos Más! Trabajamos para transformar la realidad de los que más lo necesitan.",
+                "Desde 1997 en Somos Más trabajamos con los chicos y chicas,
+                mamás y papás, abuelos y vecinos del barrio La Cava generando
+                procesos de crecimiento y de inserción social. Uniendo las manos de
+                todas las familias, las que viven en el barrio y las que viven fuera de
+                él, es que podemos pensar, crear y garantizar estos procesos. Somos
+                una asociación civil sin fines de lucro que se creó en 1997 con la
+                intención de dar alimento a las familias del barrio. Con el tiempo
+                fuimos involucrándonos con la comunidad y agrandando y mejorando
+                nuestra capacidad de trabajo. Hoy somos un centro comunitario que
+                acompaña a más de 700 personas a través de las áreas de educación, deportes, primera infancia, salud, alimentación y trabajo
+                social.",
+                "Somos_Más",
+                "Somos Mas",
+                "SomosMás",
+                current_timestamp(),
+                true);
