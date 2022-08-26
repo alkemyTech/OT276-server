b3e8da091c1fbe9c3a67f546f5784224ae5afb27
Nestor Flores <nestorchoele@gmail.com>
2022-08-26 09:55:31 -0300

    revear change

--- src/main/java/com/alkemy/ong/config/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/config/SecurityConfig.java
@@ -1,19 +1,10 @@
 package com.alkemy.ong.config;
 
-import com.alkemy.ong.config.security.JwtRequestFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.servlet.http.HttpServletResponse;
-
 
 @Configuration
 @RequiredArgsConstructor
