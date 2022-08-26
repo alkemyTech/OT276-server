ab04e6baf9a215ac97ce5bb01109953c55e861b6
Nestor Flores <nestorchoele@gmail.com>
2022-08-26 11:03:27 -0300

    [OT276-32] Añadir lógica de JWT (#18)
    * implement JwtRequestFilter
    
    * implement JwtUtils
    
    * fix subject
    
    * create jwt Request filter
    
    * create jwt utils
    
    * refactoring jwt utils
    
    * add MyUserDetailsService
    
    * implement SecurityConfig for login/register
    
    * add UserControllerMapper interface
    
    * add UserRepository
    
    * add UserRepository
    
    * i re-written code again based on https://github.com/manerajona/springmvc-authserver and video tutorial.
    
    * old
    
    implement JwtRequestFilter
    
    implement JwtUtils
    
    fix subject
    
    create jwt Request filter
    
    create jwt utils
    
    refactoring jwt utils
    
    add MyUserDetailsService
    
    implement SecurityConfig for login/register
    
    add UserControllerMapper interface
    
    add UserRepository
    
    * i re-written code again based on https://github.com/manerajona/springmvc-authserver and video tutorial.
    
    * does not correspond
    
    * does not correspond
    
    * does not correspond
    
    * does not correspond
    
    * does not correspond
    
    * does not correspond
    
    * revear change
    
    * add findByEmail
    
    * implement loadUserByUsername
    
    * secret and expiration

---
+++ src/main/java/com/alkemy/ong/core/usecase/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.alkemy.ong.core.usecase.impl;
+
+import com.alkemy.ong.core.repository.UserRepository;
+import com.alkemy.ong.core.usecase.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("username %s not found".formatted(email)));
+    }
+}
