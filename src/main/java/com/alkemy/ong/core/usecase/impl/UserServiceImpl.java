4006fd3b398d52a203dc251f130bf704a91e1761
Juacun <juanpablo.cundere@gmail.com>
2022-08-29 18:31:52 -0300

    Implementation of getList() method in UserServiceImpl

--- src/main/java/com/alkemy/ong/core/usecase/impl/UserServiceImpl.java
+++ src/main/java/com/alkemy/ong/core/usecase/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.core.usecase.impl;
 
+import com.alkemy.ong.core.model.User;
 import com.alkemy.ong.core.repository.UserRepository;
 import com.alkemy.ong.core.usecase.UserService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -15,9 +18,15 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
+    public List<User> getList() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("username %s not found".formatted(email)));
     }
+
 }
