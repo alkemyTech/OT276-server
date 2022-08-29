a60971b704a7e37cffe0fe0e7aca06596f6577c9
matiaguro <105395800+matiaguro@users.noreply.github.com>
2022-08-29 18:52:04 -0300

    [OT276-70] Creacion de endpoint Member (#20)
    * creacion de endpoint Member
    
    * Correccion de errores
    
    * Se agrega metodo createMember a interfaz MemberApi
    
    * fix error compilación

---
+++ src/main/java/com/alkemy/ong/core/usecase/MemberService.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.core.usecase;
+
+import com.alkemy.ong.core.model.Member;
+
+public interface MemberService {
+
+
+    Long createEntity(Member member);
+}
--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/AlkymerControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/AlkymerControllerMapper.java
@@ -8,7 +8,6 @@ import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
-
 import java.util.List;
 
 @Mapper
