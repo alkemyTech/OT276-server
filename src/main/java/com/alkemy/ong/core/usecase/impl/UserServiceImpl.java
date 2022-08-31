dfb9fc61d61b7a9f06a98d6ad523a0f1a0b91d5f
Juacun <juanpablo.cundere@gmail.com>
2022-08-31 17:32:08 -0300

    Implementation of updateEntityIfExists() method in UserServiceImpl

--- src/main/java/com/alkemy/ong/core/usecase/impl/UserServiceImpl.java
+++ src/main/java/com/alkemy/ong/core/usecase/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.core.usecase.impl;
 
+import com.alkemy.ong.config.exception.NotFoundException;
 import com.alkemy.ong.core.model.User;
 import com.alkemy.ong.core.repository.UserRepository;
 import com.alkemy.ong.core.usecase.UserService;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +23,20 @@ public class UserServiceImpl implements UserService {
     @Transactional(readOnly = true)
     public List<User> getList() {
         return userRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void updateEntityIfExists(Long id, User user) {
+        userRepository.findById(id)
+                .map(userJpa -> {
+                    Optional.ofNullable(user.getFirstName()).ifPresent(userJpa::setFirstName);
+                    Optional.ofNullable(user.getLastName()).ifPresent(userJpa::setLastName);
+                    Optional.ofNullable(user.getPassword()).ifPresent(userJpa::setPassword);
+                    Optional.ofNullable(user.getPhoto()).ifPresent(userJpa::setPhoto);
+
+                    return userRepository.save(userJpa);
+                }).orElseThrow(() -> new NotFoundException(id));
     }
 
     @Override
