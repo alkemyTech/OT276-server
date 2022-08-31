d0ff34ffb0bc9418b41322af8ac97ba38f80fcd9
JuanPablo <52846984+Juacun@users.noreply.github.com>
2022-08-31 13:00:31 -0300

    [OT276-36] Endpoint para listar Usuarios (#28)
    * Added getList() method to UserService
    
    * Implementation of getList() method in UserServiceImpl
    
    * Creation of UserResponse in response package
    
    * Creation of UserControllerMapper in mapper package
    
    * Creation of UserController in controller package
    
    * Added configuration for path '/v1/users' in SecurityConfig
    
    * Added Transactional to getList() in UserServiceImpl
    
    * Switch to JpaRepository in UserRepository
    
    * Refactor of getUsers() method in UserController
    
    * Added '@IterableMapping' to UserControllerMapper and Reformat Code to UserController

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.alkemy.ong.ports.input.rs.controller;
+
+import com.alkemy.ong.core.model.User;
+import com.alkemy.ong.core.usecase.UserService;
+import com.alkemy.ong.ports.input.rs.mapper.UserControllerMapper;
+import com.alkemy.ong.ports.input.rs.response.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static com.alkemy.ong.ports.input.rs.api.ApiConstants.USERS_URI;
+
+@RestController
+@RequestMapping(USERS_URI)
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService service;
+    private final UserControllerMapper mapper;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getUsers() {
+
+        List<User> list = service.getList();
+        List<UserResponse> response = mapper.userListToUserResponseList(list);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+}
---
+++ src/main/java/com/alkemy/ong/ports/input/rs/response/UserResponse.java
@@ -0,0 +1,27 @@
+package com.alkemy.ong.ports.input.rs.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+
+    private Long id;
+
+    @JsonProperty("first_Name")
+    private String firstName;
+
+    @JsonProperty("last_Name")
+    private String lastName;
+
+    private String email;
+
+    private String photo;
+
+}
