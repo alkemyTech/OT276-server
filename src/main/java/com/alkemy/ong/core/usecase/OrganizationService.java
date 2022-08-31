d17b87fdc5f039d7125d6a1690a88cca9f6296ec
Juanchini23 <91612292+Juanchini23@users.noreply.github.com>
2022-08-31 12:52:52 -0300

    [OT276-24] Endpoint datos públicos de la Organización (#26)
    * feature done
    
    * sin comentarios extras
    
    * format code
    
    * ultimo cambio pre pull
    
    * get organization
    
    * cambio requerido
    
    * cambio organization
    
    * cambio nombre repo
    
    * transactional
    
    * terminado
    
    * todo terminado

---
+++ src/main/java/com/alkemy/ong/core/usecase/OrganizationService.java
@@ -0,0 +1,8 @@
+package com.alkemy.ong.core.usecase;
+
+import com.alkemy.ong.core.model.Organization;
+
+public interface OrganizationService {
+
+    Organization getOrganizationEntity(Long id);
+}
