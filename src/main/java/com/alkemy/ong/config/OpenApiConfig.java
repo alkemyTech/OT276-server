3ba0e812ea60197f5a249c83af6c52cacdfa9ab3
Leandro Amariles <amarilespl@hotmail.com>
2022-08-31 11:06:46 -0500

    OpenApiDefinition configurado

--- src/main/java/com/alkemy/ong/config/OpenApiConfig.java
+++ src/main/java/com/alkemy/ong/config/OpenApiConfig.java
@@ -1,4 +1,23 @@
 package com.alkemy.ong.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "ONG Project", version = "v1"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class OpenApiConfig {
+
+
+
 }
