bb515acf864fcfb0e80a1da2ecd584af1274ea63
Juacun <juanpablo.cundere@gmail.com>
2022-08-31 18:03:54 -0300

    Added updateUser endpoint with path '/v1/users/{id}' to UserController

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/UserController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/UserController.java
@@ -3,13 +3,15 @@ package com.alkemy.ong.ports.input.rs.controller;
 import com.alkemy.ong.core.model.User;
 import com.alkemy.ong.core.usecase.UserService;
 import com.alkemy.ong.ports.input.rs.mapper.UserControllerMapper;
+import com.alkemy.ong.ports.input.rs.request.UpdateUserRequest;
 import com.alkemy.ong.ports.input.rs.response.UserResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 import static com.alkemy.ong.ports.input.rs.api.ApiConstants.USERS_URI;
@@ -29,6 +31,13 @@ public class UserController {
         List<UserResponse> response = mapper.userListToUserResponseList(list);
 
         return ResponseEntity.ok().body(response);
+    }
+
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateUser(@NotNull @PathVariable Long id, @Valid @RequestBody UpdateUserRequest updateUserRequest) {
+        User user = mapper.updateUserRequestToUser(updateUserRequest);
+        service.updateEntityIfExists(id, user);
     }
 
 }
