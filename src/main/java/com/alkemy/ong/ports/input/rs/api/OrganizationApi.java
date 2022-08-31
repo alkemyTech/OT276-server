446b81fed89fe2d014acf78285b610219d573776
Juanchini23 <jmanchini@alumno.unlam.edu.ar>
2022-08-31 09:49:10 -0300

    terminado

--- src/main/java/com/alkemy/ong/ports/input/rs/api/OrganizationApi.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/OrganizationApi.java
@@ -7,12 +7,11 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 
-@SecurityRequirement(name = "bearerAuth")
 @Validated
 public interface OrganizationApi {
 
     @Operation(summary = "get Organization", description = "get Organization", responses = {
-            @ApiResponse(responseCode = "204", description = "No Content")
+            @ApiResponse(responseCode = "200", description = "Ok")
     })
     ResponseEntity<OrganizationResponse> getOrganization(Long id);
 
