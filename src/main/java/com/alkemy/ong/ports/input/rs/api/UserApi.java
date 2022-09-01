402c926b9b687d381b1e6526bee6ea4376ffa63b
Leandro Amariles <amarilespl@hotmail.com>
2022-09-01 10:03:19 -0500

    login documentado

--- src/main/java/com/alkemy/ong/ports/input/rs/api/UserApi.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/UserApi.java
@@ -1,14 +1,30 @@
 package com.alkemy.ong.ports.input.rs.api;
 
+import com.alkemy.ong.config.exception.error.ErrorDetails;
 import com.alkemy.ong.ports.input.rs.request.LoginRequest;
 import com.alkemy.ong.ports.input.rs.response.AuthenticationResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
+import java.awt.*;
 
 @Validated
 public interface UserApi {
+
+    @Operation(summary = "Login user", description = "Login an exist user", responses = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+            schema = @Schema(implementation = ErrorDetails.class),
+            examples =@ExampleObject(value = "{\"code\":\"BAD_CREDENTIALS\",\"detail\":\"The server cannot return a response due to invalid credentials.\"}"))})
+    })
 
 AuthenticationResponse login(@Valid @RequestBody LoginRequest request);
 }
