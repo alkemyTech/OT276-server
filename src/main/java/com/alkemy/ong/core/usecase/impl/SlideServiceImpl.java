990b576766555e5fbda1b784f2ed250388aa25bc
Juacun <juanpablo.cundere@gmail.com>
2022-09-01 15:36:36 -0300

    Creation of SlideServiceImpl in usecase.impl package

---
+++ src/main/java/com/alkemy/ong/core/usecase/impl/SlideServiceImpl.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.core.usecase.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SlideServiceImpl {
+}
