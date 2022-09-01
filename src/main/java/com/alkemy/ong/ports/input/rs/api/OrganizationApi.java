a0e38d33ac18aa22ac3589bab9f5eb3068c887b2
Juacun <juanpablo.cundere@gmail.com>
2022-09-01 16:22:54 -0300

    Added getSlidesByOrganizationIdAndOrderByOrder() endpoint with path '/{id}/slides' to OrganizationApi

--- src/main/java/com/alkemy/ong/ports/input/rs/api/OrganizationApi.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/OrganizationApi.java
@@ -1,11 +1,14 @@
 package com.alkemy.ong.ports.input.rs.api;
 
 import com.alkemy.ong.ports.input.rs.response.OrganizationResponse;
+import com.alkemy.ong.ports.input.rs.response.SlideResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Validated
 public interface OrganizationApi {
@@ -14,5 +17,10 @@ public interface OrganizationApi {
             @ApiResponse(responseCode = "200", description = "Ok")
     })
     ResponseEntity<OrganizationResponse> getOrganization(Long id);
+
+    @Operation(summary = "get Slides filtered and order", description = "get Slides by Organization Id and order by Order", responses = {
+            @ApiResponse(responseCode = "200", description = "Ok")
+    })
+    ResponseEntity<List<SlideResponse>> getSlidesByOrganizationIdAndOrderByOrder(@NotNull Long id);
 
 }
