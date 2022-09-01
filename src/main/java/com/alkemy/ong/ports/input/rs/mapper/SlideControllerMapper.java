69b03b29a797ac6b8250bee63f4e287d60b709ad
Juacun <juanpablo.cundere@gmail.com>
2022-09-01 15:51:03 -0300

    Added slideToSlideResponse() and slideListToSlideResponseList() mappers to SlideControllerMapper

--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/SlideControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/SlideControllerMapper.java
@@ -1,7 +1,21 @@
 package com.alkemy.ong.ports.input.rs.mapper;
 
+import com.alkemy.ong.core.model.Slide;
+import com.alkemy.ong.ports.input.rs.response.SlideResponse;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Named;
+
+import java.util.List;
 
 @Mapper
-public interface SlideControllerMapper {
+public interface SlideControllerMapper extends CommonMapper {
+
+    @Named("slideToSlideResponse")
+    SlideResponse slideToSlideResponse(Slide slide);
+
+    @Named("slideListToSlideResponseList")
+    @IterableMapping(qualifiedByName = "slideToSlideResponse")
+    List<SlideResponse> slideListToSlideResponseList(List<Slide> slides);
+
 }
