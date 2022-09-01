17b3f13cb89fdef43d10efd50d746fcd117c55fa
Juacun <juanpablo.cundere@gmail.com>
2022-09-01 15:40:36 -0300

    Implementation of getListByOrganizationIdAndOrderByOrder() method in SlideServiceImpl

--- src/main/java/com/alkemy/ong/core/usecase/impl/SlideServiceImpl.java
+++ src/main/java/com/alkemy/ong/core/usecase/impl/SlideServiceImpl.java
@@ -1,9 +1,29 @@
 package com.alkemy.ong.core.usecase.impl;
 
+import com.alkemy.ong.config.exception.NotFoundException;
+import com.alkemy.ong.core.model.Slide;
+import com.alkemy.ong.core.repository.OrganizationRepository;
+import com.alkemy.ong.core.repository.SlideRepository;
+import com.alkemy.ong.core.usecase.SlideService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class SlideServiceImpl {
+public class SlideServiceImpl implements SlideService {
+
+    private final SlideRepository slideRepository;
+    private final OrganizationRepository organizationRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Slide> getListByOrganizationIdAndOrderByOrder(Long id) {
+
+        organizationRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+        return slideRepository.findByOrganizationIdOrderByOrder(id);
+    }
+
 }
