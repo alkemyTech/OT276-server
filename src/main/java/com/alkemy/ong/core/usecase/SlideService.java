404989ffe9a7dcebde08029a4aef6b17fddaca0e
Juacun <juanpablo.cundere@gmail.com>
2022-09-01 15:29:46 -0300

    Added getListByOrganizationIdAndOrderByOrder() method to SlideService

--- src/main/java/com/alkemy/ong/core/usecase/SlideService.java
+++ src/main/java/com/alkemy/ong/core/usecase/SlideService.java
@@ -1,4 +1,11 @@
 package com.alkemy.ong.core.usecase;
 
+import com.alkemy.ong.core.model.Slide;
+
+import java.util.List;
+
 public interface SlideService {
+
+    List<Slide> getListByOrganizationIdAndOrderByOrder(Long id);
+
 }
