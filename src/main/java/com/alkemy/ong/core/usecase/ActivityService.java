b1104013bcd5a5c09de682930e4d8b07024d377e
Ema_Gonzalez <emanuelgonzalezg2@gmail.com>
2022-09-02 01:37:20 -0300

    ticket OT276-51
    Creacion de clases:
    ActivityApi, ActivityRequest, ActivityService.

---
+++ src/main/java/com/alkemy/ong/core/usecase/ActivityService.java
@@ -0,0 +1,8 @@
+package com.alkemy.ong.core.usecase;
+
+import com.alkemy.ong.core.model.Activity;
+
+public interface ActivityService {
+
+    public Long createEntity(Activity activity);
+}
---
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/ActivityApi.java
@@ -0,0 +1,7 @@
+package com.alkemy.ong.ports.input.rs.api;
+
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+public interface ActivityApi {
+}
