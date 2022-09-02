bc58c8ff08fbb6a8e1dbcc8e9422b47b97ff6a5e
FedeOA <fedeacosta07@hotmail.com>
2022-09-02 09:42:48 -0300

    create ContactService

---
+++ src/main/java/com/alkemy/ong/core/usecase/impl/ContactServiceImp.java
@@ -0,0 +1,22 @@
+package com.alkemy.ong.core.usecase.impl;
+
+import com.alkemy.ong.core.model.Contact;
+import com.alkemy.ong.core.repository.ContactRepository;
+import com.alkemy.ong.core.usecase.ContactService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ContactServiceImp implements ContactService {
+
+    ContactRepository contactRepository;
+
+    @Override
+    @Transactional
+    public Long createEntity(Contact contact) {
+
+        return contactRepository.save(contact).getId();
+    }
+}
