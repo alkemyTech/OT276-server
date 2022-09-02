6053da249136f232fe053db762ab93112115261a
FedeOA <fedeacosta07@hotmail.com>
2022-09-02 09:43:25 -0300

    create ContactRequest

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/request/CreateContactRequest.java
@@ -0,0 +1,32 @@
+package com.alkemy.ong.ports.input.rs.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateContactRequest {
+
+    @NotBlank(message = "Complete with your name")
+    @JsonProperty("name")
+    private String name;
+
+    @NotBlank(message = "complete with your phone")
+    @JsonProperty("phone")
+    private int phone;
+
+    @NotBlank(message = "Complete with your email")
+    @JsonProperty("email")
+    private String email;
+
+    @NotBlank(message = "complete with a message")
+    @JsonProperty("message")
+    private String message;
+}
