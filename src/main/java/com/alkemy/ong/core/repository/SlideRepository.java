b7a1cfea79b1030e07f8d85f37ae16de32301651
Nestor Flores <nestorchoele@gmail.com>
2022-09-04 15:09:29 -0300

    implement findFirstByOrderByIdDesc

--- src/main/java/com/alkemy/ong/core/repository/SlideRepository.java
+++ src/main/java/com/alkemy/ong/core/repository/SlideRepository.java
@@ -3,5 +3,8 @@ package com.alkemy.ong.core.repository;
 import com.alkemy.ong.core.model.Slide;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SlideRepository extends JpaRepository<Slide, Long> {
+    Optional<Slide> findFirstByOrderByIdDesc();
 }
