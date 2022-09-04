fb59c56f62630f5dea11ff8e306eb6e0a075c61a
Nestor Flores <nestorchoele@gmail.com>
2022-09-04 15:12:08 -0300

    add createSlide

--- src/main/java/com/alkemy/ong/ports/input/rs/api/SlideApi.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/SlideApi.java
@@ -1,13 +1,21 @@
 package com.alkemy.ong.ports.input.rs.api;
 
+import com.alkemy.ong.ports.input.rs.request.SlideRequest;
 import com.alkemy.ong.ports.input.rs.response.SlideResponse;
 import com.sun.istack.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
 
 
 @Validated
 public interface SlideApi {
 
     ResponseEntity<SlideResponse> getById(@NotNull Long id);
+
+    @PostMapping
+    ResponseEntity<Void> createSlide(@Valid @RequestBody SlideRequest slideRequest);
 }
