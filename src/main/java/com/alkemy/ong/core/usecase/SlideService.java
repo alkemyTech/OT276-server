23a13498f93aba2e5c2355df5cdd9c36a4796889
Nestor Flores <nestorchoele@gmail.com>
2022-09-04 15:08:50 -0300

    add createEntity

--- src/main/java/com/alkemy/ong/core/usecase/SlideService.java
+++ src/main/java/com/alkemy/ong/core/usecase/SlideService.java
@@ -1,10 +1,14 @@
 package com.alkemy.ong.core.usecase;
 
 import com.alkemy.ong.core.model.Slide;
+import com.alkemy.ong.ports.input.rs.request.SlideRequest;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface SlideService {
 
     Slide getSlideEntity(Long id);
 
+    @Transactional
+    long createEntity(SlideRequest slideRequest);
 
 }
