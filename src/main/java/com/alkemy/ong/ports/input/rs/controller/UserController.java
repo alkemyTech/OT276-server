9f2d93ba56db1fc1e40fbe4b87d7a07981d59cba
Ema_Gonzalez <emanuelgonzalezg2@gmail.com>
2022-09-04 19:33:35 -0300

    Update UserController.java
    Ese metodo ya estaba en el AuthController pero tambien estaba en el UserController.

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/UserController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/UserController.java
@@ -5,9 +5,7 @@ import com.alkemy.ong.core.usecase.UserService;
 import com.alkemy.ong.ports.input.rs.mapper.UserControllerMapper;
 import com.alkemy.ong.ports.input.rs.response.UserResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,12 +21,6 @@ public class UserController {
 
     private final UserService service;
     private final UserControllerMapper mapper;
-
-    @GetMapping("/me")
-    public ResponseEntity<UserResponse> getUserInformation(@AuthenticationPrincipal User user) {
-        UserResponse userResponse = mapper.userToUserResponse(user);
-        return new ResponseEntity<>(userResponse, HttpStatus.OK);
-    }
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> getUsers() {
