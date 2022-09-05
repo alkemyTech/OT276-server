7c73cfdfa30a849ed877b09f15eaa7a2606a1d8c
Leandro Amariles <amarilespl@hotmail.com>
2022-09-05 11:06:30 -0500

    feature/OT276-90

--- src/main/java/com/alkemy/ong/ports/input/rs/api/UserApi.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/UserApi.java
@@ -36,24 +36,28 @@ public interface UserApi {
             @ApiResponse(responseCode = "401", description = "Unauthorized",
             content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
             schema = @Schema(implementation = ErrorDetails.class),
-            examples =@ExampleObject(value = "{\"code\":\"BAD_CREDENTIALS\",\"detail\":\"The server cannot return a response due to invalid credentials.\"}"))})
+            examples = @ExampleObject(value = "{\"code\":\"BAD_CREDENTIALS\",\"detail\":\"The server cannot return a response due to invalid credentials.\"}"))})
     })
     AuthenticationResponse login(@Valid @RequestBody LoginRequest request);
 
     @Operation(summary = "Register user", description = "Register a new user", responses = {
             @ApiResponse(responseCode = "200", description = "Ok"),
-            @ApiResponse(responseCode = )
-    })
+            @ApiResponse(responseCode = "409", description = "Conflict",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+            schema = @Schema(implementation = ErrorDetails.class),
+            examples = @ExampleObject(value = "{\"code\":\"RESOURCE_ALREADY_EXISTS\",\"detail\":\"There is already an account with that email address:\"}"))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+            schema = @Schema(implementation =  ErrorDetails.class),
+            examples = @ExampleObject(value = "{\"code\":\"INVALID_FIELD_VALUE\",\"detail\":\"Password dont must be empty or email must be a correctly format e-mail address\"}"))})
 
+    })
     ResponseEntity<?> register(@Valid @RequestBody CreateUserRequest userRequest);
 
-    @Operation(summary = "Register user", description = "Register a new user", responses = {
-            @ApiResponse(responseCode = "200", description = "Ok"),
-            @ApiResponse(responseCode = )
+    @Operation(summary = "User profile", description = "Get information about actual user", responses = {
+            @ApiResponse(responseCode = "200", description = "Ok")
     })
-
-
     ResponseEntity<UserResponse> getUserInformation(@AuthenticationPrincipal User user);
-    
+
 
 }
