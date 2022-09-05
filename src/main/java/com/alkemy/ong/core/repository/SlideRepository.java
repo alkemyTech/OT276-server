81e37d9abe22d1a05725037b40e11e6330af04f7
Nestor Flores <nestorchoele@gmail.com>
2022-09-04 22:56:17 -0300

    fix conflict

--- src/main/java/com/alkemy/ong/core/repository/SlideRepository.java
+++ src/main/java/com/alkemy/ong/core/repository/SlideRepository.java
@@ -3,8 +3,10 @@ package com.alkemy.ong.core.repository;
 import com.alkemy.ong.core.model.Slide;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SlideRepository extends JpaRepository<Slide, Long> {
+    List<Slide> findByOrganizationIdOrderByOrder(Long id);
     Optional<Slide> findFirstByOrderByIdDesc();
 }
