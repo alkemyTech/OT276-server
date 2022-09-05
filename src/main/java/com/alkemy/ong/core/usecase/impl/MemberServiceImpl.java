7600f1aaa08524942a9bc82a918b812f6ba47e09
matiaguro <105395800+matiaguro@users.noreply.github.com>
2022-09-05 20:28:40 -0300

    Agregar paginación a Miembros

--- src/main/java/com/alkemy/ong/core/usecase/impl/MemberServiceImpl.java
+++ src/main/java/com/alkemy/ong/core/usecase/impl/MemberServiceImpl.java
@@ -1,10 +1,15 @@
 package com.alkemy.ong.core.usecase.impl;
 
 import com.alkemy.ong.config.exception.NotFoundException;
+import com.alkemy.ong.core.model.Alkymer;
+import com.alkemy.ong.core.model.AlkymerList;
 import com.alkemy.ong.core.model.Member;
+import com.alkemy.ong.core.model.MemberList;
 import com.alkemy.ong.core.repository.MemberRepository;
 import com.alkemy.ong.core.usecase.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -41,4 +46,10 @@ public class MemberServiceImpl implements MemberService {
         memberRepository.findById(id).ifPresent(memberRepository::delete);
     }
 
+    @Override
+    @Transactional(readOnly = true)
+    public MemberList getList(PageRequest pageRequest) {
+        Page<Member> page = memberRepository.findAll(pageRequest);
+        return new MemberList(page.getContent(), pageRequest, page.getTotalElements());
+    }
 }
