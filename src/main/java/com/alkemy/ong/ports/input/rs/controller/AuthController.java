17b2cb93f889de2ad3bc78ca33e5fef2e728b498
Jonathan Manera <manerajona@gmail.com>
2022-09-05 20:15:38 -0300

    Fix imports AuthController

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/AuthController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/AuthController.java
@@ -9,6 +9,7 @@ import com.alkemy.ong.ports.input.rs.request.CreateUserRequest;
 import com.alkemy.ong.ports.input.rs.request.LoginRequest;
 import com.alkemy.ong.ports.input.rs.response.AuthenticationResponse;
 import com.alkemy.ong.ports.input.rs.response.RegisterResponse;
+import com.alkemy.ong.ports.input.rs.response.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
