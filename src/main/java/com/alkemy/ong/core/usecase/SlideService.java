4ca9a42933b3b5c5a988d2097eec8694877157c0
Nestor Flores <nestorchoele@gmail.com>
2022-09-04 22:56:33 -0300

    fix conflict

--- src/main/java/com/alkemy/ong/core/usecase/SlideService.java
+++ src/main/java/com/alkemy/ong/core/usecase/SlideService.java
@@ -4,9 +4,13 @@ import com.alkemy.ong.core.model.Slide;
 import com.alkemy.ong.ports.input.rs.request.SlideRequest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface SlideService {
 
     Slide getSlideEntity(Long id);
+
+    List<Slide> getListByOrganizationIdAndOrderByOrder(Long id);
 
     @Transactional
     long createEntity(SlideRequest slideRequest);
