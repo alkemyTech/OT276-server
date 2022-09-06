176826e6c269392f4fd0f5d48a3326eb3d47b76c
Nestor Flores <nestorchoele@gmail.com>
2022-09-05 22:24:46 -0300

    refactoring code with deprecated functions.

--- src/main/java/com/alkemy/ong/config/security/JwtUtils.java
+++ src/main/java/com/alkemy/ong/config/security/JwtUtils.java
@@ -2,11 +2,13 @@ package com.alkemy.ong.config.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -35,7 +37,7 @@ public class JwtUtils {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+        return Jwts.parserBuilder().setSigningKey(getKey()).build().parseClaimsJws(token).getBody();
     }
 
     private Boolean isTokenExpired(String token) {
@@ -47,10 +49,16 @@ public class JwtUtils {
         return createToken(claims, userDetails.getUsername());
     }
 
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
     private String createToken(Map<String, Object> claims, String subject) {
+
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+                .signWith(getKey()).compact();
     }
 
     public Boolean validateToken(String token, UserDetails userDetails) {
