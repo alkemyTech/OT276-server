483b79449d4ebacfd0e3b3ffe22f64bfd7f7f65e
matiaguro <105395800+matiaguro@users.noreply.github.com>
2022-09-07 19:43:50 -0300

    [OT276-83] Agregar paginación a Miembros (#49)
    * Agregar paginación a Miembros
    
    * Cambios solicitados

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/MemberController.java
@@ -1,25 +1,34 @@
 package com.alkemy.ong.ports.input.rs.controller;
 
 import com.alkemy.ong.core.model.Member;
-import com.alkemy.ong.core.usecase.impl.MemberServiceImpl;
+import com.alkemy.ong.core.model.MemberList;
+import com.alkemy.ong.core.usecase.MemberService;
+import com.alkemy.ong.ports.input.rs.api.ApiConstants;
 import com.alkemy.ong.ports.input.rs.api.MemberApi;
 import com.alkemy.ong.ports.input.rs.mapper.MemberControllerMapper;
 import com.alkemy.ong.ports.input.rs.request.MemberRequest;
+import com.alkemy.ong.ports.input.rs.response.MemberResponse;
+import com.alkemy.ong.ports.input.rs.response.MemberResponseList;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 import static com.alkemy.ong.ports.input.rs.api.ApiConstants.MEMBERS_URI;
 
@@ -29,7 +38,7 @@ import static com.alkemy.ong.ports.input.rs.api.ApiConstants.MEMBERS_URI;
 public class MemberController implements MemberApi {
 
     private final MemberControllerMapper mapper;
-    private  final MemberServiceImpl memberService;
+    private  final MemberService memberService;
 
     @Override
     @PostMapping
@@ -59,4 +68,32 @@ public class MemberController implements MemberApi {
             memberService.deleteById(id);
 
         }
+
+    @GetMapping
+    public ResponseEntity<MemberResponseList> getMember(@RequestParam Optional<Integer> page,
+                                                        @RequestParam Optional<Integer> size) {
+
+        final int pageNumber = page.filter(p -> p > 0).orElse(ApiConstants.DEFAULT_PAGE);
+        final int pageSize = size.filter(s -> s > 0).orElse(ApiConstants.DEFAULT_PAGE_SIZE);
+
+        MemberList list = memberService.getList(PageRequest.of(pageNumber, pageSize));
+
+        MemberResponseList response;
+        {
+            response = new MemberResponseList();
+
+            List<MemberResponse> content = mapper.memberListToMemberResponseList(list.getContent());
+            response.setContent(content);
+
+            final int nextPage = list.getPageable().next().getPageNumber();
+            response.setNextUri(ApiConstants.uriByPageAsString.apply(nextPage));
+
+            final int previousPage = list.getPageable().previousOrFirst().getPageNumber();
+            response.setPreviousUri(ApiConstants.uriByPageAsString.apply(previousPage));
+
+            response.setTotalPages(list.getTotalPages());
+            response.setTotalElements(list.getTotalElements());
+        }
+        return ResponseEntity.ok().body(response);
+    }
 }
--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/MemberControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/MemberControllerMapper.java
@@ -2,11 +2,15 @@ package com.alkemy.ong.ports.input.rs.mapper;
 
 import com.alkemy.ong.core.model.Member;
 import com.alkemy.ong.ports.input.rs.request.MemberRequest;
+import com.alkemy.ong.ports.input.rs.response.MemberResponse;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface MemberControllerMapper extends CommonMapper {
 
     Member memberRequestToMember(MemberRequest memberRequest);
 
+    List<MemberResponse> memberListToMemberResponseList(List<Member> members);
 }
