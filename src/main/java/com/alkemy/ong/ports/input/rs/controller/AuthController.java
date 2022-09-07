3bc048c71e3b50d273e4b1c0ec6f23bd07fcb060
Jonathan Manera <manerajona@gmail.com>
2022-09-07 14:46:12 -0300

    fix controller

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/AuthController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/AuthController.java
@@ -3,7 +3,7 @@ package com.alkemy.ong.ports.input.rs.controller;
 import com.alkemy.ong.config.security.JwtUtils;
 import com.alkemy.ong.core.model.User;
 import com.alkemy.ong.core.usecase.UserService;
-import com.alkemy.ong.ports.input.rs.api.UserApi;
+import com.alkemy.ong.ports.input.rs.api.AuthApi;
 import com.alkemy.ong.ports.input.rs.mapper.UserControllerMapper;
 import com.alkemy.ong.ports.input.rs.request.CreateUserRequest;
 import com.alkemy.ong.ports.input.rs.request.LoginRequest;
@@ -21,12 +21,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-
 
 import javax.validation.Valid;
 import java.util.Date;
@@ -36,7 +34,7 @@ import static com.alkemy.ong.ports.input.rs.api.ApiConstants.AUTHENTICATION_URI;
 @RestController
 @RequestMapping(AUTHENTICATION_URI)
 @RequiredArgsConstructor
-public class AuthController implements UserApi {
+public class AuthController implements AuthApi {
 
     private final AuthenticationManager authenticationManager;
 
