f35ced61c8abef2c2efc64a83cce49ec60466d7a
Nestor Flores <nestorchoele@gmail.com>
2022-09-09 00:39:50 -0300

    add updateSlideIfExist

--- src/main/java/com/alkemy/ong/core/usecase/SlideService.java
+++ src/main/java/com/alkemy/ong/core/usecase/SlideService.java
@@ -11,4 +11,7 @@ public interface SlideService {
     List<Slide> getListByOrganizationIdAndOrderByOrder(Long id);
 
     long createEntity(String imageBase64, Integer order, String text, Long organizationId);
+
+    void updateEntityIfExists(Long id, Long organizationId, String imageBase64, Integer order, String text);
+
 }
