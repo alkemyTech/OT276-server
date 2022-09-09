1a6f7bae055e43e1a19b55e7cc97e1ad3f97b5dc
FedeOA <fedeacosta07@hotmail.com>
2022-09-09 09:45:36 -0300

    Create responses of new, comment and user

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/response/NewCommentsResponse.java
@@ -0,0 +1,19 @@
+package com.alkemy.ong.ports.input.rs.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewCommentsResponse {
+
+    private long id;
+
+    private Set<CommentResponse> comments;
+}
