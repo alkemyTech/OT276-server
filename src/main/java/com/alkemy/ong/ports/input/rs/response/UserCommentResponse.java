d2359ac0c87d03899e8bc6b35f150897781afb01
FedeOA <fedeacosta07@hotmail.com>
2022-09-12 20:28:02 -0300

    Change UserCommentResponse

--- src/main/java/com/alkemy/ong/ports/input/rs/response/UserCommentResponse.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/response/UserCommentResponse.java
@@ -11,5 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserCommentResponse {
 
-    private String email;
+    private String firstName;
+
+    private String lastName;
 }
