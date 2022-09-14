9e07d6e74250b654d293df910cf67dac815e401a
Nestor Flores <nestorchoele@gmail.com>
2022-09-13 21:35:44 -0300

    add documentation

--- src/main/java/com/alkemy/ong/ports/input/rs/api/TestimonialApi.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/TestimonialApi.java
@@ -2,10 +2,11 @@ package com.alkemy.ong.ports.input.rs.api;
 
 import com.alkemy.ong.config.exception.error.ErrorDetails;
 import com.alkemy.ong.ports.input.rs.request.TestimonialRequest;
-
+import com.alkemy.ong.ports.input.rs.response.AlkymerResponseList;
+import com.alkemy.ong.ports.input.rs.response.TestimonialResponse;
 import com.alkemy.ong.ports.input.rs.response.TestimonialResponseList;
-
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,14 +15,12 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-
 import java.util.Optional;
 
 
@@ -50,7 +49,7 @@ public interface TestimonialApi {
     })
     ResponseEntity<Void> createTestimonial(@Valid @RequestBody TestimonialRequest request);
 
-    @Operation(summary = "Create Testimonial", description = "Create a new Testimonial event", responses = {
+    @Operation(summary = "Delete Testimonial", description = "Delete a Testimonial event", responses = {
             @ApiResponse(responseCode = "204", description = "No Content"),
             @ApiResponse(responseCode = "401", description = "Unauthorized",
                     content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
@@ -63,9 +62,36 @@ public interface TestimonialApi {
     })
     void deleteTestimonial(@NotNull @RequestParam long id);
 
-
+    @Operation(summary = "Get Testimonial List", description = "Get Testimonial List", responses = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = AlkymerResponseList.class))}),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"BAD_CREDENTIALS\",\"detail\":\"The server cannot return a response due to invalid credentials.\"}"))}),
+    })
     ResponseEntity<TestimonialResponseList> getTestimonialList(@RequestParam Optional<Integer> page,
                                                                @RequestParam Optional<Integer> size);
 
-
+    @Operation(summary = "update  Testimonial", description = "update  Testimonial", responses = {
+            @ApiResponse(responseCode = "204", description = "No Content"),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            array = @ArraySchema(schema = @Schema(implementation = ErrorDetails.class)),
+                            examples = @ExampleObject(value = "[{\"code\":\"INVALID_FIELD_VALUE\",\"detail\":\"must not be blank\",\"field\":\"name\",\"location\":\"BODY\"}]"))}),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"BAD_CREDENTIALS\",\"detail\":\"The server cannot return a response due to invalid credentials.\"}"))}),
+            @ApiResponse(responseCode = "403", description = "Forbidden",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"ROLE_INVALID\",\"detail\":\"The user does not have access to the current resource\"}"))}),
+            @ApiResponse(responseCode = "404", description = "Not Found",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"RESOURCE_NOT_FOUND\",\"detail\":\"The resource with id 99 is not found\"}"))}),
+    })
+    ResponseEntity<TestimonialResponse> updateTestimonial(@Valid @PathVariable Long id, @RequestBody TestimonialRequest request);
 }
