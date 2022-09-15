443215201290089d0fe05849d5655668b48dae8f
Ema_Gonzalez <99846234+EmaGonzalezxd@users.noreply.github.com>
2022-09-15 09:37:44 -0300

    [OT276-51] Endpoint Creación Novedades. (#73)
    * cambios
    
    * .

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/NewController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/NewController.java
@@ -1,8 +1,10 @@
 package com.alkemy.ong.ports.input.rs.controller;
 
+import com.alkemy.ong.core.model.New;
 import com.alkemy.ong.core.usecase.NewService;
 import com.alkemy.ong.ports.input.rs.api.NewApi;
 import com.alkemy.ong.ports.input.rs.mapper.NewControllerMapper;
+import com.alkemy.ong.ports.input.rs.request.CreateNewRequest;
 import com.alkemy.ong.ports.input.rs.response.CommentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.net.URI;
 import java.util.List;
 
 import static com.alkemy.ong.ports.input.rs.api.ApiConstants.NEWS_URI;
@@ -25,6 +32,7 @@ public class NewController implements NewApi {
 
     private final NewControllerMapper mapper;
 
+
     @Override
     @GetMapping("/{id}/comments")
     public ResponseEntity<List<CommentResponse>> getComments(@NotNull @PathVariable Long id) {
@@ -32,5 +40,21 @@ public class NewController implements NewApi {
         List<CommentResponse> response = mapper.commentListToCommentResponseList(newService.getComments(id));
 
         return ResponseEntity.ok(response);
+    }
+
+
+    @Override
+    @PostMapping("")
+    public ResponseEntity<Void> createEntity(@Valid @RequestBody CreateNewRequest createNewRequest) {
+
+        New _new= mapper.createNewRequestToNew(createNewRequest);
+
+        final long id = newService.createEntity(_new,createNewRequest.getCategoryId());
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(id)
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/NewControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/NewControllerMapper.java
@@ -1,8 +1,11 @@
 package com.alkemy.ong.ports.input.rs.mapper;
 
 import com.alkemy.ong.core.model.Comment;
+import com.alkemy.ong.core.model.New;
+import com.alkemy.ong.ports.input.rs.request.CreateNewRequest;
 import com.alkemy.ong.ports.input.rs.response.CommentResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 import java.util.List;
@@ -12,4 +15,6 @@ public interface NewControllerMapper extends CommonMapper {
 
     @Named("commentListToCommentResponseList")
     List<CommentResponse> commentListToCommentResponseList(List<Comment> comments);
+
+    New createNewRequestToNew(CreateNewRequest createNewRequest);
 }
