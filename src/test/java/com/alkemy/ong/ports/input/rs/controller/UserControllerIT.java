ea2a156907fe1737f018ffaab0d20011d9d6795e
Juacun <juanpablo.cundere@gmail.com>
2022-09-16 10:05:07 -0300

    Creation of UserControllerIT

---
+++ src/test/java/com/alkemy/ong/ports/input/rs/controller/UserControllerIT.java
@@ -0,0 +1,17 @@
+package com.alkemy.ong.ports.input.rs.controller;
+
+import com.alkemy.ong.H2Config;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@SpringJUnitConfig(classes = H2Config.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class UserControllerIT {
+}
