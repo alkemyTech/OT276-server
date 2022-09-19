8b1b925b933a93c51e44b89bf7cdf5d50939e3c9
matiaguro <105395800+matiaguro@users.noreply.github.com>
2022-09-19 20:15:52 -0300

    Correccion en el tipeo de los imports

--- src/test/java/com/alkemy/ong/ports/input/rs/controller/MemberControllerTest.java
+++ src/test/java/com/alkemy/ong/ports/input/rs/controller/MemberControllerTest.java
@@ -29,7 +29,10 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
