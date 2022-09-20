62d6efda7115b7456f430e5fc4687a6bc97d663c
Ema_Gonzalez <emanuelgonzalezg2@gmail.com>
2022-09-20 09:02:10 -0300

    Update NewControllerTest.java

--- src/test/java/com/alkemy/ong/ports/input/rs/controller/NewControllerTest.java
+++ src/test/java/com/alkemy/ong/ports/input/rs/controller/NewControllerTest.java
@@ -1,8 +1,14 @@
 package com.alkemy.ong.ports.input.rs.controller;
 
 import com.alkemy.ong.config.exception.handler.GlobalExceptionHandler;
+import com.alkemy.ong.config.util.JsonUtils;
+import com.alkemy.ong.core.model.Category;
+import com.alkemy.ong.core.model.New;
 import com.alkemy.ong.core.usecase.NewService;
+import com.alkemy.ong.ports.input.rs.api.ApiConstants;
 import com.alkemy.ong.ports.input.rs.mapper.NewControllerMapper;
+import com.alkemy.ong.ports.input.rs.request.CreateNewRequest;
+import com.alkemy.ong.ports.input.rs.response.NewResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,10 +17,19 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class NewControllerTest {
@@ -39,14 +54,60 @@ class NewControllerTest {
 
     @Test
     void createEntity_shouldReturn201() throws Exception {
+        CreateNewRequest request = CreateNewRequest.builder()
+                .name("foo")
+                .content("bar")
+                .image("foo")
+                .categoryId(1L)
+                .build();
 
+        given(service.createEntity(any(New.class))).willReturn(22L);
+
+        final String actualLocation = mockMvc.perform(post(ApiConstants.NEWS_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.objectToJson(request)))
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getHeader(HttpHeaders.LOCATION);
+
+        assertThat(actualLocation).isEqualTo("http://localhost/v1/news/22");
     }
 
     @Test
-    void getNew() {
+    void getNew_shouldReturn200() throws Exception {
+        Category category = new Category();
+        category.setName("politica");
+
+        New news = new New();
+        news.setId(22L);
+        news.setName("foo");
+        news.getCategory();
+
+        given(service.getByIdIfExists(22L)).willReturn(news);
+
+        String content = mockMvc.perform(get(ApiConstants.NEWS_URI + "/22"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        assertThat(content).isNotBlank();
+
+        NewResponse response = JsonUtils.jsonToObject(content, NewResponse.class);
+
+        assertThat(response.getId()).isEqualTo(22);
+        assertThat(response.getName()).isEqualTo("foo");
+        assertThat(response.getCategory()).isNotEmpty();
+        assertThat(response.getCategory().stream().findFirst().orElseThrow().getName()).isEqualTo("politica");
     }
 
     @Test
-    void deleteNew() {
+    void deleteNew_shouldReturn204() throws Exception {
+        mockMvc.perform(delete(ApiConstants.NEWS_URI + "/22"))
+                .andExpect(status().isNoContent())
+                .andDo(print());
     }
 }
