eb47c2d7b4d706740869a7ad5213171e9be37dc4
Ema_Gonzalez <emanuelgonzalezg2@gmail.com>
2022-09-20 09:14:52 -0300

    ticket 82

--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/NewControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/NewControllerMapper.java
@@ -5,6 +5,7 @@ import com.alkemy.ong.core.model.New;
 import com.alkemy.ong.ports.input.rs.request.CreateNewRequest;
 import com.alkemy.ong.ports.input.rs.response.CommentResponse;
 import com.alkemy.ong.ports.input.rs.response.NewResponse;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
@@ -23,5 +24,6 @@ public interface NewControllerMapper extends CommonMapper {
     @Mapping(source = "category.name", target = "category")
     NewResponse newToNewResponse(New news);
 
+    @IterableMapping(qualifiedByName = "CategoryToCategoryResponse")
     List<NewResponse> newListToNewResponseList(List<New> newList);
 }
