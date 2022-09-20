449d85ea6e923ff7c6fe4a010ebcaa3aee3ffedd
FedeOA <fedeacosta07@hotmail.com>
2022-09-19 21:51:56 -0300

    add documentation to deleteNew

--- src/main/java/com/alkemy/ong/ports/input/rs/api/NewApi.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/NewApi.java
@@ -74,5 +74,16 @@ public interface NewApi {
     })
     ResponseEntity<NewResponse> getNew(@NotNull Long id);
 
+    @Operation(summary = "Delete  New", description = "Delete  New", responses = {
+            @ApiResponse(responseCode = "204", description = "No Content"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"BAD_CREDENTIALS\",\"detail\":\"The server cannot return a response due to invalid credentials.\"}"))}),
+            @ApiResponse(responseCode = "403", description = "Forbidden",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"ROLE_INVALID\",\"detail\":\"The user does not have access to the current resource\"}"))}),
+    })
     void deleteNew(@NotNull Long id);
 }
