dcb3a48d5457497e4968dcca2341d93ed21e2337
Leandro <101153802+Yeyini7615@users.noreply.github.com>
2022-09-21 14:12:57 -0500

    [OT276-99] Test: Endpoints /testimonials (#79)
    * 5/09/22
    
    * tests/OT276-99
    
    * IT running
    
    * tests/OT276-99 19/09
    
    * tests/OT276-99 test fixed and working

--- src/main/java/com/alkemy/ong/ports/input/rs/api/TestimonialApi.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/TestimonialApi.java
@@ -2,6 +2,7 @@ package com.alkemy.ong.ports.input.rs.api;
 
 import com.alkemy.ong.config.exception.error.ErrorDetails;
 import com.alkemy.ong.ports.input.rs.request.TestimonialRequest;
+import com.alkemy.ong.ports.input.rs.request.UpdateTestimonialRequest;
 import com.alkemy.ong.ports.input.rs.response.TestimonialResponse;
 import com.alkemy.ong.ports.input.rs.response.TestimonialResponseList;
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,11 +16,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
 
@@ -59,7 +59,7 @@ public interface TestimonialApi {
                             schema = @Schema(implementation = ErrorDetails.class),
                             examples = @ExampleObject(value = "{\"code\":\"ROLE_INVALID\",\"detail\":\"The user does not have access to the current resource \"}"))})
     })
-    void deleteTestimonial(@NotNull @RequestParam long id);
+    void deleteTestimonial(@PathVariable Long id);
 
     @Operation(summary = "Get Testimonial List", description = "Get Testimonial List", responses = {
             @ApiResponse(responseCode = "200", description = "OK",
@@ -70,8 +70,8 @@ public interface TestimonialApi {
                             schema = @Schema(implementation = ErrorDetails.class),
                             examples = @ExampleObject(value = "{\"code\":\"BAD_CREDENTIALS\",\"detail\":\"The server cannot return a response due to invalid credentials.\"}"))}),
     })
-    ResponseEntity<TestimonialResponseList> getTestimonialList(@RequestParam Optional<Integer> page,
-                                                               @RequestParam Optional<Integer> size);
+    ResponseEntity<TestimonialResponseList> getTestimonialList(@PathVariable Optional<Integer> page,
+                                                               @PathVariable Optional<Integer> size);
 
     @Operation(summary = "Update Testimonial", description = "Update Testimonial", responses = {
             @ApiResponse(responseCode = "200", description = "OK"),
@@ -92,7 +92,7 @@ public interface TestimonialApi {
                             schema = @Schema(implementation = ErrorDetails.class),
                             examples = @ExampleObject(value = "{\"code\":\"RESOURCE_NOT_FOUND\",\"detail\":\"The resource with id 99 is not found\"}"))}),
     })
-    ResponseEntity<TestimonialResponse> updateTestimonial(@Valid @PathVariable Long id, @RequestBody TestimonialRequest request);
-
+    ResponseEntity<TestimonialResponse> updateTestimonial(@Valid @PathVariable Long id, @RequestBody UpdateTestimonialRequest request);
 
 }
+
---
+++ src/test/java/com/alkemy/ong/ports/input/rs/controller/TestimonialControllerIT.java
@@ -0,0 +1,162 @@
+package com.alkemy.ong.ports.input.rs.controller;
+
+import com.alkemy.ong.H2Config;
+import com.alkemy.ong.config.util.JsonUtils;
+import com.alkemy.ong.ports.input.rs.api.ApiConstants;
+import com.alkemy.ong.ports.input.rs.request.TestimonialRequest;
+import com.alkemy.ong.ports.input.rs.response.TestimonialResponse;
+import com.alkemy.ong.ports.input.rs.response.TestimonialResponseList;
+import org.apache.http.HttpHeaders;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@SpringJUnitConfig(classes = H2Config.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestimonialControllerIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @Order(1)
+    @WithUserDetails("admin@somosmas.org")
+    void createTestimonial_shouldReturn201() throws Exception {
+
+        TestimonialRequest request = TestimonialRequest.builder()
+                .name("testi")
+                .content("content 1")
+                .build();
+        final String actualLocation = mockMvc.perform(post(ApiConstants.TESTIMONIALS_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.objectToJson(request)))
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getHeader(HttpHeaders.LOCATION);
+
+        assertThat(actualLocation).isEqualTo("http://localhost/v1/testimonials/1");
+
+    }
+
+    @Test
+    @Order(2)
+    @WithUserDetails("admin@somosmas.org")
+    void getTestimonial_shouldReturn200() throws Exception {
+
+        String content = mockMvc.perform(get(ApiConstants.TESTIMONIALS_URI))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        assertThat(content).isNotBlank();
+
+        TestimonialResponseList response = JsonUtils.jsonToObject(content, TestimonialResponseList.class);
+
+        assertThat(response.getTotalElements()).isEqualTo(1);
+        assertThat(response.getTotalPages()).isEqualTo(1);
+        assertThat(response.getNextUri()).isEqualTo("http://localhost/v1/testimonials?page=1");
+        assertThat(response.getPreviousUri()).isEqualTo("http://localhost/v1/testimonials?page=0");
+        assertThat(response.getContent()).isNotNull();
+
+    }
+
+    @Test
+    @Order(3)
+    @WithUserDetails("admin@somosmas.org")
+    void updateTestimonial_shouldReturn200() throws Exception {
+
+        TestimonialRequest request = TestimonialRequest.builder()
+                .name("Updated Name")
+                .content("New Content")
+                .image("Nueva Imagen")
+                .build();
+
+        String content = mockMvc.perform(put(ApiConstants.TESTIMONIALS_URI + "/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.objectToJson(request)))
+                .andExpect(status().isOk()).andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+
+        TestimonialResponse response = JsonUtils.jsonToObject(content, TestimonialResponse.class);
+
+        assertThat(response).isInstanceOf(TestimonialResponse.class);
+        assertThat(response.getName()).isEqualTo("Updated Name");
+        assertThat(response.getContent()).isEqualTo("New Content");
+        assertThat(response.getImage()).isEqualTo("Nueva Imagen");
+
+    }
+
+    @Test
+    @Order(4)
+    @WithUserDetails("admin@somosmas.org")
+    void deleteTestimonial_shouldReturn204() throws Exception {
+        mockMvc.perform(delete(ApiConstants.TESTIMONIALS_URI + "/{id}", 1L))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+    }
+
+    @Test
+    @Order(5)
+    @WithUserDetails("jdoe@somosmas.org")
+    void getTEstimonial_shouldReturn404() throws Exception {
+
+        String content = mockMvc.perform(get(ApiConstants.TESTIMONIALS_URI))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        TestimonialResponseList response = JsonUtils.jsonToObject(content, TestimonialResponseList.class);
+
+        assertThat(response.getTotalElements()).isEqualTo(0);
+    }
+
+    @Test
+    @Order(6)
+    @WithUserDetails("jdoe@somosmas.org")
+    void deleteTestimonial_shouldReturn403() throws Exception {
+        mockMvc.perform(delete(ApiConstants.TESTIMONIALS_URI))
+                .andExpect(status().isForbidden())
+                .andDo(print());
+    }
+
+    @Test
+    @Order(7)
+    @WithAnonymousUser
+    void geTestimonial_shouldReturn401() throws Exception {
+        mockMvc.perform(get(ApiConstants.TESTIMONIALS_URI))
+                .andExpect(status().isUnauthorized())
+                .andDo(print());
+    }
+
+
+}
