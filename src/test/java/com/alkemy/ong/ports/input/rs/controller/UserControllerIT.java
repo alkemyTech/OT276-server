8c06c6a02d789f9e5c573e111eb6b627e8a547f6
Jonathan Manera <manerajona@gmail.com>
2022-09-21 16:26:39 -0300

    DirtiesContext

--- src/test/java/com/alkemy/ong/ports/input/rs/controller/UserControllerIT.java
+++ src/test/java/com/alkemy/ong/ports/input/rs/controller/UserControllerIT.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,6 +29,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@DirtiesContext
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
